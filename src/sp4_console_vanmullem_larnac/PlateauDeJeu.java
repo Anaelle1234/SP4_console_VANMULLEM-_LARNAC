1bad2df40ef86c1224b739c8a73d3483fa322da6
anaelle larnac <anaelle.larnac@epfedu.fr>
2022-11-28 12:27:18 +0100

    creation classe plateau de jeu + joueur

---
+++ src/sp4_console_vanmullem_larnac/PlateauDeJeu.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_vanmullem_larnac;
+
+/**
+ *
+ * @author Anaëlle
+ */
+public class PlateauDeJeu {
+    CelluleDeGrille[][] grille = new CelluleDeGrille [6][7];
+
+    public PlateauDeJeu() {
+    }
+    
+}
---
+++ src/sp4_console_vanmullem_larnac/joueur.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_vanmullem_larnac;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Anaëlle
+ */
+public class joueur {
+    private String nom;
+    private String couleur;
+    private ArrayList<Jeton> reserveJetons = new ArrayList<Jeton>();
+    private int nombreDesintegrateurs;
+
+    public joueur(String nom, int nombreDesintegrateurs) {
+        nombreDesintegrateurs=0;
+        this.nom = nom;
+        this.nombreDesintegrateurs = nombreDesintegrateurs;
+    }
+ public AffecterCouleur(String)
+}
