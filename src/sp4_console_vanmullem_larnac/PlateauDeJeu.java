8c409602047d54115fca364f54f027475209c003
anaelle larnac <anaelle.larnac@epfedu.fr>
2022-12-02 10:24:41 +0100

    partie plateau de jeu jusqu'à etreGagnantePourCouleur(String)

--- src/sp4_console_vanmullem_larnac/PlateauDeJeu.java
+++ src/sp4_console_vanmullem_larnac/PlateauDeJeu.java
@@ -19,6 +19,18 @@ public class PlateauDeJeu {
         }  
     }
     
+    public int ajouterJetonDansColonne(Jeton j, int c){
+        for(int i=0;i<7;i++){
+            if(grille[i][c].presenceJeton()==false){
+                grille[i][c].affecterJeton(j);
+                return i;
+            }
+            
+        }
+   return -1;     
+    }
+    
+    
     public boolean grilleRempli(){
         int cpt=0;
         for(int i=0;i<=6;i++){
@@ -35,27 +47,56 @@ public class PlateauDeJeu {
     }   
     }
         
-    public CelluleDeGrille afficherGrilleSurConsole(){
-    }
 
-    @Override
-    public String toString() {
+    public String afficherGrilleSurConsole(){
         return "PlateauDeJeu{" + "grille=" + grille + '}';
     }
 
-    
 public boolean presenceJeton(int x, int y){
-if (grille[x][y]!=null){
+    if (grille[x][y]!=null){
     return true;
 }else{
     return false;
 }
 }
 public String lireCouleurDuJeton(int x, int y){
-    grille[x][y].lireCouleurDuJeton();
-    return grille[x][y].lireCouleurDuJeton();
+    grille[x][y].LireCouleurDuJeton();
+    return grille[x][y].LireCouleurDuJeton();
 }
-public etreGagnantePourCouleur(String){
+
+public void tasserColonne(int a){
+    
+}
+public boolean colonneRemplie(int b){
+    
+        return false;
+    
+}
+public boolean presenceTrouNoir(int x, int y ){
+    
+        return false;
+    
+} 
+public void placerTrouNoir(int x, int y){
+    
+}
+public void supprimerTrouNoir(int x, int y ){
+    
+} 
+public void placerDesintegrateur(int x,int y){
+    
+}
+public void supprimerDesintegrateur(int x,int y){
+    
+}
+public boolean presenceDesintegrateur (int x, int y){
+    
+        return false;
+}
+public void supprimerJeton(int x, int y){
+    
+}
+public void recupererJeton(int x, int y){
     
 }
 }
