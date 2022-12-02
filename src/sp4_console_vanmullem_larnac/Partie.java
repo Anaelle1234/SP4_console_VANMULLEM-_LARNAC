51224e43ce359ca0d43b1f499b20ab4c7afffc3a
Solene_Van_Mullem <solene.vml@gmail.com>
2022-12-02 09:01:43 +0100

    Attributs créés Méthode Partie Méthode attribuerCouleurAuxJoueurs Début de creerEtAffecterJeton(Joueur)

--- src/sp4_console_vanmullem_larnac/Partie.java
+++ src/sp4_console_vanmullem_larnac/Partie.java
@@ -4,10 +4,41 @@
  */
 package sp4_console_vanmullem_larnac;
 
+import java.util.Random;
+
 /**
  *
  * @author Soso
  */
 public class Partie {
     
+    private String listeJoueurs[]= new String[2]; //tableau référence des deux joueurs 
+    private joueur joueurCourant;
+    private PlateauDeJeu plateau;
+    
+    public void Partie(joueur Joueur) {
+        listeJoueurs[0]="Joueur";
+        listeJoueurs[1]="Joueur";
+        
+        plateau = new PlateauDeJeu();
+        
+    }
+    
+    public void attribuerCouleurAuxJoueurs() {
+        Random n = new Random();
+        int nbr = n.nextInt(2); // choisie aléatoirement entre 1 et 2
+        
+        if (nbr==1) {
+            listeJoueurs[0] = "Rouge";
+            listeJoueurs[1] = "Jaune";            
+        }else{ // si nbr==2
+            listeJoueurs[0] = "Jaune";
+            listeJoueurs[1] = "Rouge";
+        }
+    }
+    
+    public void creerEtAffecterJeton(joueur Joueur) {
+        
+        
+    }
 }
