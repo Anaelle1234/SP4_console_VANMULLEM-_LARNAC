1b45d16fede735828bb984d91bf81e02db1195f1
Solene_Van_Mullem <solene.vml@gmail.com>
2022-12-04 18:19:44 +0100

    Ensemble du code écrit sauf : dans Plateau de jeu il manque la methode "etre gagnante pour couleur" revoir la methode tasser colonne revoir dans la classe partie la nomination des deux joueurs dans la methode initialiser partie et revoir la methode lancer partie on niveau du switch

--- src/sp4_console_vanmullem_larnac/PlateauDeJeu.java
+++ src/sp4_console_vanmullem_larnac/PlateauDeJeu.java
@@ -36,6 +36,7 @@ public class PlateauDeJeu {
     }
         
     public CelluleDeGrille afficherGrilleSurConsole(){
+        return null;
     }
 
     @Override
@@ -55,7 +56,71 @@ public String lireCouleurDuJeton(int x, int y){
     grille[x][y].lireCouleurDuJeton();
     return grille[x][y].lireCouleurDuJeton();
 }
-public etreGagnantePourCouleur(String){
+//public etreGagnantePourCouleur(String){ // A REMPLIR
     
 }
+
+public void tasserColonne(int col) {
+    int nb_ligne = 0;
+    for(int i = 0; i < 8; i++){
+        if(grille[i][col] == null){
+            for (i = 0; i < 8; i++) {
+                if (grille[i][col] == null) {
+                    nb_ligne = i;
+                    break;
+                }
+            }
+            for(i = nb_ligne; i < 8; i++){
+                if(grille[i][col] != null){
+                    grille[i-1][col] = grille[i][col];
+                        for (i = nb_ligne; i < 8; i++) { //REVOIR...
+                            if (grille[i][col] != null) {
+                                grille[i - 1][col] = grille[i][col];
+                            }
+                        }
+                }
+            }
+        }
+    }
 }
+
+    public boolean presenceTrouNoir(int x, int y){
+        return(grille[x][y].presenceTrouNoir());
+    }
+    
+    public void placerTrouNoir(int x, int y){
+        grille[x][y].placerTrouNoir();
+    }
+    
+    public void supprimerTrouNoir(int x, int y){
+        grille[x][y].supprimerTrouNoir();
+    }
+    
+    public boolean presenceDesintegrateur(int x, int y){
+        return(grille[x][y].presenceDesintegrateur());
+    }
+
+    public void placerDesintegrateur(int x, int y) {
+        grille[x][y].placerDesintegrateur();
+    }
+
+    public void supprimeresentDegrateur(int x, int y) {
+        grille[x][y].supprimerDesintegrateur();
+    }
+
+    public void supprimerJeton(int x, int y) {
+        grille[x][y].supprimerJeton();
+    }
+
+    public Jeton recupererJeton(int x, int y){
+        return(grille[x][y].recupererJeton());
+    }
+
+    public String lireJeton(int x, int y) {
+        return (grille[x][y].LireCouleurJeton());
+    }
+}
+            
+        
+    
+                
